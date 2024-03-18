8b9f36c1c83618341fa621a1d7c7c13866cf5cff
ratherforky <jf16688@bristol.ac.uk>
2024-03-18 19:00:41 +0000

    added reader monad code

--- misc/src/MoreMonads.hs
+++ misc/src/MoreMonads.hs
@@ -26,3 +26,64 @@ instance Monad Tree where
 
 join :: Monad m => m (m a) -> m a
 join mmx = mmx >>= id
+
+newtype Reader a b = Reader { runReader :: a -> b }
+
+newtype Reader' a b = Reader' (a -> b)
+runReader' :: Reader' a b -> (a -> b)
+runReader' (Reader' f) = f
+
+eg1, eg2, eg3 :: Reader String Int
+eg1 = Reader{runReader = length}
+eg2 = Reader length
+eg3 = Reader (\x -> 5)
+
+instance Functor (Reader a) where
+  fmap :: (b -> c) -> Reader a b -> Reader a c
+  fmap f (Reader g) = Reader (f . g)
+
+
+instance Applicative (Reader e) where
+  pure :: a -> Reader e a
+  pure x = Reader $ const x
+  (<*>) :: Reader e (a -> b) -> Reader e a -> Reader e b
+  Reader f <*> Reader g = Reader $ \e -> f e (g e)
+
+instance Monad (Reader e) where
+  (>>=) :: Reader e a -> (a -> Reader e b) -> Reader e b
+  Reader f >>= g = Reader $ \e -> runReader (g (f e)) e
+
+egDo :: Reader a a
+egDo = do
+  env <- ask
+  pure env 
+
+egDo' :: a -> a
+egDo' = \x -> x
+
+ask :: Reader e e
+ask = Reader $ \x -> x
+
+-- silly bullshit you can do with the Functor instance for `(->) e`
+-- because `fmap` is `(.)` for the unwrapped reader monad...
+-- >>> fmap (+ 1) (* 10) 0
+-- 1
+-- >>> fmap (* 10) (+ 1) 0
+-- 10
+-- >>> (* 10) `fmap` (+ 1) $ 0
+-- 10
+
+-- two different fmaps!
+-- >>> fmap (fmap (+ 1) (* 10)) [1..3]
+-- [11,21,31]
+
+-- honestly i don't even know what's going on here...
+-- >>> fmap fmap fmap (+ 1) [[1,2],[3,4]]
+-- [[2,3],[4,5]]
+
+-- you can just keep adding fmaps to create pointless new horrors:
+-- >>> fmap fmap fmap fmap fmap (+ 1) (* 10) [1..3]
+-- [11,21,31]
+-- >>> fmap fmap fmap fmap fmap fmap fmap fmap fmap fmap fmap fmap fmap fmap (+) [0,1,2] <*> [[1,2,3]]
+-- [[1,2,3],[2,3,4],[3,4,5]]
+-- (again, please don't do this)
