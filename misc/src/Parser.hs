476da018f1b29c43bd16a82564eadf5b70d0d9dc
Samantha Frohlich <sf16540@my.bristol.ac.uk>
2024-12-05 12:30:43 +0000

    final session

--- misc/src/Parser.hs
+++ misc/src/Parser.hs
@@ -1,16 +1,33 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Parser where
 
-import Text.ParserCombinators.ReadP
+-- import Text.ParserCombinators.ReadP
 import Data.Char (isDigit)
+import Control.Arrow
 
-type Parser = ReadP
+-- type Parser = ReadP
 
-parse :: Parser a -> String -> [(a, String)]
-parse p str = readP_to_S p str
+-- parse :: Parser a -> String -> [(a, String)]
+-- parse p str = readP_to_S p str
 
-digit :: Parser Int
-digit = fmap (read . pure) (satisfy isDigit)
+-- digit :: Parser Int
+-- digit = fmap (read . pure) (satisfy isDigit)
 
-int :: Parser Int
--- int :: ReadP [Char]
-int = fmap read $ many1 (satisfy isDigit)
+-- int :: Parser Int
+-- -- int :: ReadP [Char]
+-- int = fmap read $ many1 (satisfy isDigit)
+
+-- based on yoda - https://hackage.haskell.org/package/yoda
+
+newtype Parser a = Parser (String -> [(a, String)])
+
+item :: Parser Char
+item = Parser (\case
+  [] -> []
+  (t:ts') -> [(t, ts')])
+
+instance Functor Parser where
+  -- fmap :: (a -> b) -> Parser a -> Parser b
+  fmap f (Parser p)
+    = Parser (\ts -> fmap (first f) (p ts))
